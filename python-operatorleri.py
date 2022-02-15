4a35b7d097fd2c6d78b148195c532c1b1ef8dfcc
Berdan Can Yetkiner <13546853+berdancanyet@users.noreply.github.com>
2022-02-15 21:12:17 +0300

    Update python-operatorleri.py

--- python-operatorleri.py
+++ python-operatorleri.py
@@ -41,3 +41,109 @@ print(x ** y) #2*2*2*2*2 ile eşdeğerdir. 32 çıktısını verir.
 x = 15
 y = 2
 print(x // y) # 7 çıktısını verir. Sonucu en yakın tam sayıya yuvarlar.
+
+#____________________________________________________________________
+# Atama Operatörleri
+
+# =
+x = 5 # x, 5'e eşit.
+print(x)
+
+# +=
+x = 5
+x += 3 # x'e 3 ekle.
+print(x)
+
+# -=
+x = 5
+x -= 3 # x'ten 3 çıkar.
+print(x)
+
+# *=
+x = 5
+x *= 3 # x'i 3'le çarp.
+print(x)
+
+# /=
+x = 5
+x /= 3 # x'i 3'e böl.
+print(x)
+
+# %=
+x = 5
+x%=3 # x'in 3 ile bölümünden kalan.
+print(x)
+
+# //=
+x = 5
+x//=3 # 5'i 3'e böl ve bölümü en yakın tam sayıya yuvarla.
+print(x)
+
+# **=
+x = 5
+x **= 3 # 5 üssü 3
+print(x)
+
+# &=
+x = 5
+x &= 3 # Bitwise AND uygular ve değeri sol işlenene atar.
+print(x)
+
+# |=
+x = 5
+x |= 3 # Bitwise OR uygular ve değeri sol işlenene atar.
+print(x)
+
+# ^=
+x = 5
+x ^= 3 # Bitwise XOR uygular ve değeri sol işlenene atar.
+print(x)
+
+# >>=
+x = 5
+x >>= 3 # Bitwise sağa kaydırma uygular ve değeri sol işlenene atar.
+print(x)
+
+# <<=
+x = 5
+x <<= 3 # Bitwise sola kaydırma uygular ve değeri sol işlenene atar.
+print(x)
+
+#____________________________________________________________________
+# Karşılaştırma Operatörleri
+
+# Eşit (==)
+# Örnek: x == y
+x = 5
+y = 3
+print(x == y) # false çıktısını verecek çünkü x ve y birbirine eşit değil.
+
+# Eşit Değildir (!=)
+# Örnek: x != y
+x = 5
+y = 3
+print(x != y) # true çıktısını verecek çünkü x ve y birbirine eşit değil.
+
+# Büyüktür (>)
+# Örnek: x > y
+x = 5
+y = 3
+print(x > y) # true çıktısını verecek çünkü x, y'den büyük.
+
+# Küçüktür (<)
+# Örnek: x < y
+x = 5
+y = 3
+print(x < y) # false çıktısını verecek çünkü x, y'den küçük değil.
+
+# Büyük Eşittir (>=)
+# Örnek: x >= y
+x = 5
+y = 3
+print(x >= y) # true çıktısını verecek çünkü x, y'den büyük veya eşittir.
+
+# Küçük Eşittir (<=)
+# Örnek: x <= y
+x = 5
+y = 3
+print(x <= y) # false çıktısını verecek çünkü x, y'ye eşit veya ondan küçük değil.
